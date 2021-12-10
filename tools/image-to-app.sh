b1e4630aa0610daaba3b6d8d3bba1c83a3241749
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-12-10 17:26:47 +0100

    some more work on template and tooling

--- tools/image-to-app.sh
+++ tools/image-to-app.sh
@@ -1,13 +1,20 @@
-for i in $(cat ../to-app.txt); do
+# The format of the to-app.txt file is as follows:
+# PORT:tccr.io/truecharts/CHART:TAG
+
+for i in $(cat to-app.txt); do
 
     PLAIN=${i##*/}
+    PORT=$( echo $i | cut -d':' -f1 )
     PLAIN=$( echo $PLAIN | cut -d':' -f1 )
     TAG=$( echo $PLAIN | cut -d':' -f2 )
     CLEANTAG=${TAG//$'\r'/}
+    CLEANPORT=${PORT//$'\r'/}
     CLEAN=${PLAIN//$'\r'/}
     echo ${CLEAN##*|}
-    cp -rf ../templates/app/* ../charts/dev/${CLEAN}
-    sed -i "s|CHARTNAME|${CLEAN}|g" ../charts/dev/${CLEAN}/Chart.yaml
-    sed -i "s|CHARTNAME|${CLEAN}|g" ../charts/dev/${CLEAN}/values.yaml
-    sed -i "s|CHARTTAG|${CLEANTAG}|g" ../charts/dev/${CLEAN}/values.yaml
+    cp -rf templates/app/* charts/dev/${CLEAN}
+    sed -i "s|CHARTNAME|${CLEAN}|g" charts/dev/${CLEAN}/Chart.yaml
+    sed -i "s|CHARTNAME|${CLEAN}|g" charts/dev/${CLEAN}/values.yaml
+    sed -i "s|CHARTTAG|${CLEANTAG}|g" charts/dev/${CLEAN}/values.yaml
+    sed -i "s|CHARTPORT|${CLEANPORT}|g" charts/dev/${CLEAN}/values.yaml
+    sed -i "s|CHARTPORT|${CLEANPORT}|g" charts/dev/${CLEAN}/questions.yaml
     done
